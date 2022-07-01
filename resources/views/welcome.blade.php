d6b6e456cf1cdbf2f61b1059e8723860c3e61cf0
Mike Joshua Espiritu <imikespiritu@gmail.com>
2022-07-01 17:46:44 +0800

    First Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,73 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pagsasanay V2</title>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>HR</title>
 </head>
 <body>
     <nav>
         <div class="container">
-            <a href="" class="logo">
-                <img src="https://picsum.photos/50?grayscale" alt="">
-            </a>
+            <a href=""><img src="" alt="logo" class="logo"></a>
             <div class="links">
-                <a href="#">About Us</a>
-                <a href="#">Store</a>
-                <a href="#">Inspiration</a>
                 <a href="#">Contact Us</a>
+                <a href="#">About Us</a>
+                <a href="#">Home</a>
             </div>
-            <div class="buttons-wrapper">
-                <button><img src="https://picsum.photos/20?random=15" alt=""></button>
-                <button><img src="https://picsum.photos/20?random=98" alt=""></button>
-            </div>
+            <div class="cta"></div>
         </div>
     </nav>
-
-    <div id="app">
-        <section class="hero">
-            <div class="container">
-                <div class="left">
-                    <div class="social-wrapper">
-                        <a href="#"><img src="https://picsum.photos/20?random=1" alt=""></a>
-                        <a href="#"><img src="https://picsum.photos/20?random=2" alt=""></a>
-                        <a href="#"><img src="https://picsum.photos/20?random=3" alt=""></a>
-                    </div>
-                    <div class="heading-texts">
-                        <h2>Lorem ipsum dolor sit amet</h2>
-                        <h1>DESIGN</h1>
-                        {{-- <img src="https://picsum.photos/100" alt=""> --}}
-                        <div class="description">
-                            <h3>Lorem.</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa illum maiores neque fugiat quaerat fuga omnis illo ut repudiandae architecto quis tenetur ea repellendus, ipsum, deleniti quisquam sed nulla voluptas. Ea magni qui ab debitis quas explicabo incidunt, laboriosam vel nostrum repudiandae provident dignissimos nisi repellat perferendis nihil cupiditate. Ducimus.</p>
-                        </div>
-                        <div class="buttons-wrapper">
-                            <button>Discover</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="right">
-                    <div class="columns-wrapper">
-                        @for($i = 0; $i < 3; $i++)
-                            <div class="box">
-                                <div class="real-box">
-                                    <div class="background">
-                                        <img src="https://picsum.photos/500?random={{ $i+1 }}" alt="">
-                                        <h2>TITLE {{ $i + 1 }}</h2>
-                                    </div>
-                                </div>
-                            </div>
-                        @endfor
-                    </div>
-                    <div class="actions-wrapper">
-                        <img src="https://picsum.photos/30?grayscale" alt="">
-                        <img src="https://picsum.photos/30?grayscale" alt="">
-                    </div>
-                </div>
-            </div>
-        </section>
-    </div>
 </body>
 </html>
